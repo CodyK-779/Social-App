b3a04baf958d871705cc5bb07cc7d498356ad38b
CodyK-779 <CodyK9627@example.com>
2025-05-29 11:11:24 +0630

    Add create post functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { ThemeProvider } from "@/components/ui/ThemeProvider";
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -42,11 +44,14 @@ export default function RootLayout({
               <Navbar />
               <main className="py-8 max-w-7xl mx-auto px-4">
                 <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
-                  <div className="hidden lg:block lg:col-span-3">sidebar</div>
+                  <div className="hidden lg:block lg:col-span-3">
+                    <Sidebar />
+                  </div>
                   <div className="lg:col-span-9">{children}</div>
                 </div>
               </main>
             </div>
+            <Toaster />
           </ThemeProvider>
         </body>
       </html>
